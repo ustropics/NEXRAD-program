cf6fb5ab05addc66fa56025d562acff8422a9cc9
USTropics <ustropics@gmail.com>
2024-10-25 04:12:52 -0400

    update to utils

--- utils.py
+++ utils.py
@@ -1,3 +1,9 @@
-def test_func(widget_box):
-    print(widget_box)
-
+def widget_values(widget_box):
+  print(widget_box.children[1].children[0].value)
+  print(widget_box.children[1].children[1].value)
+  print(widget_box.children[2].children[0].value)
+  print(widget_box.children[2].children[1].value)
+  print(widget_box.children[3].children[0].value)
+  print(widget_box.children[3].children[1].value)
+  print(widget_box.children[4].children[0].value)
+  print(widget_box.children[4].children[1].value)
