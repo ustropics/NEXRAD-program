259fd99d5a041348144c13ba1b689af2c5682841
USTropics <ustropics@gmail.com>
2024-10-25 10:36:51 -0400

    update to plotting data

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from utils import *
 
-def test_func():
+def test_func(widget_box):
     dt1, dt2, lon_1, lon_2, lat_1, lat_2 = widget_values(widget_box)
     print(dt1, dt2, lon_1, lon_2, lat_1, lat_2)
